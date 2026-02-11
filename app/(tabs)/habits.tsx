4a13a6a8c907991cf1020440b6003c5524bb0ccd
ishan.developerguide <ishan.developerguide@gmail.com>
2026-02-11 09:12:05 +0000

    Re-organize Habit Screen to Include Add Button

--- app/(tabs)/habits.tsx
+++ app/(tabs)/habits.tsx
@@ -1,13 +1,13 @@
+import AddButton from '@/components/AddButton';
 import HabitList from '@/components/HabitList';
 import globalStyles from '@/styles/globals';
 import { View } from 'react-native';
 
 export default function HabitsPage() {
     return (
-        <View 
-            style={globalStyles.container}
-        >
+        <View style={{ flex: 1 }} >
             <HabitList />
+            <AddButton/>
         </View>
     );
 }
